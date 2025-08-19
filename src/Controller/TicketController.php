ee9b018af4e3e94a9e8323682f1917eeee6d49ce
arizzo <anjorizz@gmail.com>
2025-08-19 12:09:38 -0400

    working on ticket view

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Ticket;
 use App\Entity\User;
 use App\Form\NewTicketType;
+use App\Repository\StatusRepository;
 use App\Repository\UserRepository;
 use App\Service\TicketService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -49,7 +51,7 @@ final class TicketController extends AbstractController
                 'tags' => $data['tags'],
             ]);
 
-            return $this->redirectToRoute('app_ticket_show', ['id' => $ticket->getId()]);
+            return $this->redirectToRoute('app_ticket_show', ['id' => $ticket?->getId()]);
         }
 
         return $this->render('ticket/new.html.twig', [
@@ -72,16 +74,101 @@ final class TicketController extends AbstractController
      * Show a ticket.
      */
     #[Route('/{id}', name: 'show')]
-    public function showTicket(UserRepository $userRepository, int $id): Response
+    public function showTicket(StatusRepository $statusRepository, UserRepository $userRepository, int $id): Response
     {
-//        $assignableUsers = $userRepository->getAssignableUsers('ticket');
+        //        $assignableUsers = $userRepository->getAssignableUsers('ticket');
         $assignableUsers = $userRepository->findAll();
+        $statuses = $statusRepository->findAll();
 
         $ticket = $this->ticketService->getTicket($id);
 
         return $this->render('ticket/show.html.twig', [
             'ticket' => $ticket,
             'assignableUsers' => $assignableUsers,
+            'statuses' => $statuses,
+        ]);
+    }
+
+    #[Route('/update/assign', name: 'update_assign', methods: ['POST'])]
+    public function ajaxAssignUser(Request $request, TicketService $ticketService, UserRepository $userRepository, #[CurrentUser] User $currentUser): JsonResponse
+    {
+        $userId = (int) $request->request->get('userId');
+
+        $ticketId = (int) $request->request->get('ticketId');
+
+        if (!$userId || !$ticketId) {
+            return new JsonResponse([
+                'success' => false,
+                'payload' => [
+                    'message' => 'Invalid user id or ticket id',
+                    'data' => [
+                        'ticketId' => $ticketId,
+                        'userId' => $userId,
+                    ],
+                ],
+            ]);
+        }
+
+        $user = $userRepository->find($userId);
+
+        if (!$user) {
+            return new JsonResponse([
+                'success' => false,
+                'payload' => [
+                    'message' => 'User not found',
+                    'data' => [
+                        'ticketId' => $ticketId,
+                        'userId' => $userId,
+                    ],
+                ],
+            ]);
+        }
+
+        $ticket = $ticketService->getTicket($ticketId);
+
+        if (!$ticket) {
+            return new JsonResponse([
+                'success' => false,
+                'payload' => [
+                    'message' => 'Ticket not found',
+                    'data' => [
+                        'ticketId' => $ticketId,
+                        'userId' => $userId,
+                    ],
+                ],
+            ]);
+        }
+
+        $ticket->setAssignedUser($user);
+
+        $ticket = $ticketService->updateTicket($ticket, $currentUser);
+
+        if ($ticket?->getAssignedUser()?->getId() !== $userId) {
+            return new JsonResponse([
+                'success' => false,
+                'payload' => [
+                    'message' => 'User not assigned successfully',
+                    'data' => [
+                        'ticketId' => $ticketId,
+                        'userId' => $userId,
+                    ],
+                ],
+            ]);
+        }
+
+        return new JsonResponse([
+            'success' => true,
+            'payload' => [
+                'message' => 'User assigned successfully',
+                'data' => [
+                    'ticketId' => $ticketId,
+                    'userId' => $userId,
+                    'user' => [
+                        'firstName' => $user->getFirstName(),
+                        'lastName' => $user->getLastName(),
+                    ],
+                ],
+            ],
         ]);
     }
 }
