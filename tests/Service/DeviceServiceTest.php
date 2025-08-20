1180c4c1268d3544fa7f8fad283a9653e2457b25
Anthony Rizzo <anjorizz@gmail.com>
2025-08-19 20:10:38 -0400

    working on device tests, had to add public: true in services.yaml for tests

--- tests/Service/DeviceServiceTest.php
+++ tests/Service/DeviceServiceTest.php
@@ -4,64 +4,24 @@ namespace App\Tests\Service;
 
 use App\Entity\Device;
 use App\Service\DeviceService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class DeviceServiceTest extends KernelTestCase
 {
-    private DeviceService $deviceService;
-
-    private EntityManagerInterface $entityManager;
-
-    protected function setUp(): void
+    public function testCreate()
     {
         self::bootKernel();
 
         $container = static::getContainer();
+        $deviceService = $container->get(DeviceService::class);
 
-        $this->deviceService = $container->get(DeviceService::class);
-
-        $this->entityManager = $container->get('doctrine')->getManager();
-
-        parent::setUp();
-    }
-
-    public function testCreate(): void
-    {
-        $data = [
-            'assetTag' => 1007812,
-            'serialNumber' => 'JAHANGIR7',
-        ];
-
-        $device = $this->deviceService->create($data);
+        $device = $deviceService->create();
 
         $this->assertInstanceOf(Device::class, $device);
     }
 
-    public function testCreateExpectException(): void
+    public function testGetDeviceById()
     {
-        $data = [
-            'assetTag' => 1007812,
-            'datePurchased' => '2024-04-12',
-        ];
 
-        $this->expectException(\TypeError::class);
-
-        $device = $this->deviceService->create($data);
-    }
-
-    public function testGetDevice(): void
-    {
-        $device = new Device();
-
-        $date = new \DateTimeImmutable();
-
-        $device->setDateCreated($date);
-
-        $this->entityManager->persist($device);
-
-        $this->entityManager->flush();
-
-        $this->assertInstanceOf(Device::class, $this->deviceService->getDeviceById($device->getId()));
     }
 }
