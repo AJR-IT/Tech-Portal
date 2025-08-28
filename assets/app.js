52fbcfae9bc026e1e3848da9a28e7bcfbfc772c8
ajrockr <anjorizz@gmail.com>
2025-08-27 20:35:09 -0400

    working on ui

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
-import './styles/app.css';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
+// import './styles/app.css';
+//
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
 import './vendor/bootstrap/dist/js/bootstrap.bundle.min.js';
 import './js/theme_switcher.js';
